0e570a6221d1533097ca887bb868f0f68008fd63
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-01-11 20:28:59 -0400

    Added Change Level
    commit

---
+++ Assets/Scripts/Interactive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Interactive : MonoBehaviour
+{
+    private Collider2D myCollider;
+    public UnityEvent onInteraction;
+
+    private void Start() {
+        myCollider = GetComponent<Collider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        onInteraction?.Invoke();
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadNextLevel(){
+
+
+        int lastIndex = SceneManager.sceneCountInBuildSettings;
+        Debug.Log(lastIndex);
+
+        int actualIndex = SceneManager.GetActiveScene().buildIndex;
+
+        int nextIndex = ++actualIndex;
+        
+        if(lastIndex != nextIndex + 1) SceneManager.LoadScene(nextIndex);
+
+    }
+}
